61791118d67bad55a3d3c110eab3444f648d30d9
olawalecodes <iwaloyeo@gmail.com>
2021-11-20 14:47:44 +0100

    UI almost done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>#todo</h1>
+      <main>
+        <div className="tabs">
+          <span>All</span>
+          <span>Active</span>
+          <span>Completed</span>
+        </div>
+        <hr />
+        <div class="add-items">
+          <input placeholder="add details" name="todo" />
+          <button>Add</button>
+        </div>
+        <div className="item">
+          <input type="checkbox" />
+          <span>Do Coding challenges</span>
+        </div>
+      </main>
+    </div >
   );
 }
 
